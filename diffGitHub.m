d4a387fca147dbba7671a9c88dd6c8a5989ec3a8
rnakhoul <rnakhoul@uk-rnakhoul.dhcp.mathworks.com>
2022-03-31 16:47:38 +0100

    change to pdf oublish

--- diffGitHub.m
+++ diffGitHub.m
@@ -1,5 +1,4 @@
 !git show HEAD:models/f14_airframe.slx > airframe_orig.slx
 comp=visdiff("airframe_orig","models/f14_airframe.slx");
 filter(comp, 'unfiltered');
-file = publish(comp)
-web(file)
+file = publish(comp,'pdf')
