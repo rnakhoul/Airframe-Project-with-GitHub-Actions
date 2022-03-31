38517fd627957b31f8e8f19c14085fa7eb661e8b
rnakhoul <rnakhoul@uk-rnakhoul.dhcp.mathworks.com>
2022-03-31 15:23:38 +0100

    fix script

--- diffGitHub.m
+++ diffGitHub.m
@@ -1,4 +1,4 @@
-!git show HEAD~1:models/f14_airframe.slx > airframe_orig.slx
+!git show HEAD:models/f14_airframe.slx > airframe_orig.slx
 comp=visdiff("airframe_orig","f14_airframe.slx");
 filter(comp, 'unfiltered');
 file = publish(comp)
