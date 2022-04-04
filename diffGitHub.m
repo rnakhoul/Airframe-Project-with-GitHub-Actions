4a4b2d65612ba7d7e43c014c878347f7994c24b3
rnakhoul <rnakhoul@uk-rnakhoul.dhcp.mathworks.com>
2022-04-04 13:23:36 +0100

    head

--- diffGitHub.m
+++ diffGitHub.m
@@ -1,4 +1,4 @@
-!(git show HEAD~1:models/f14_airframe.slx > models/airframe_orig.slx)
+!(git show HEAD:models/f14_airframe.slx > models/airframe_orig.slx)
 comp=visdiff("models/airframe_orig.slx","models/f14_airframe.slx");
 filter(comp, 'unfiltered');
 file = publish(comp,'pdf')
