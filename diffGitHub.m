6173f7a276ce708c7850b1b5cc0bdf9fd407b7f2
rnakhoul <rnakhoul@uk-rnakhoul.dhcp.mathworks.com>
2022-04-05 10:19:18 +0100

    change model

--- diffGitHub.m
+++ diffGitHub.m
@@ -1,3 +1,6 @@
+[status,modifiedFiles] = system("git diff --name-only HEAD~1..HEAD '*.slx' ");
+modifiedFiles = split(modifiedFiles)
+modifiedFiles = modifiedFiles(1:(end-1))
 !(git show HEAD~1:models/f14_airframe.slx > models/airframe_orig.slx)
 comp=visdiff("models/airframe_orig.slx","models/f14_airframe.slx");
 filter(comp, 'unfiltered');
