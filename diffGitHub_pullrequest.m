21a079dde9bd732ffeea9de767d4d6739d13290d
rnakhoul <51159223+rnakhoul@users.noreply.github.com>
2023-06-09 15:06:55 +0100

    Update diffGitHub_pullrequest.m

--- diffGitHub_pullrequest.m
+++ diffGitHub_pullrequest.m
@@ -5,12 +5,12 @@ function diffGitHub_pullrequest(branchname)
     % List modified models since branch diverged from main
     % Use *** to search recursively for modified SLX files starting in the current folder
     % git diff --name-only refs/remotes/origin/main..refs/remotes/origin/branchtomerge
-    gitCommand = sprintf('git diff --name-only refs/remotes/origin/main..refs/remotes/origin/%s ***.slx', branchname);
-    [status,modifiedFiles] = system(gitCommand);
+    gitCommand = sprintf('git diff --name-only refs/remotes/origin/main..refs/remotes/origin/%s ***.slx', branchname)
+    [status,modifiedFiles] = system(gitCommand)
     assert(status==0, modifiedFiles);
     
     modifiedFiles = split(modifiedFiles);
-    modifiedFiles(end) = []; % Removing last element because it is empty
+    modifiedFiles(end) = [] % Removing last element because it is empty
     
     if isempty(modifiedFiles)
         disp('No modified models to compare.')
@@ -20,12 +20,12 @@ function diffGitHub_pullrequest(branchname)
     % Create a temporary folder to store the ancestors of the modified models
     % If you have models with the same name in different folders, consider
     % creating multiple folders to prevent overwriting temporary models
-    tempdir = fullfile(proj.RootFolder, "modelscopy");
+    tempdir = fullfile(proj.RootFolder, "modelscopy")
     mkdir(tempdir)
     
     % Generate a comparison report for every modified model file
     for i = 1:numel(modifiedFiles)
-        report = diffToAncestor(tempdir,string(modifiedFiles(i)));
+        report = diffToAncestor(tempdir,string(modifiedFiles(i)))
     end
     
     % Delete the temporary folder
@@ -41,9 +41,9 @@ function report = diffToAncestor(tempdir,fileName)
 
     % Compare models and publish results in a printable report
     % Specify the format using 'pdf', 'html', or 'docx'
-    comp= visdiff(ancestor, fileName);
+    comp= visdiff(ancestor, fileName)
     filter(comp, 'unfiltered');
-    report = publish(comp,'html');
+    report = publish(comp,'html')
     
 end
 
@@ -58,10 +58,10 @@ function ancestor = getAncestor(tempdir,fileName)
     ancestor = strrep(sprintf('%s%s%s',ancestor, "_ancestor", ext), '\', '/');
     % Build git command to get ancestor from main
     % git show refs/remotes/origin/main:models/modelname.slx > modelscopy/modelname_ancestor.slx
-    gitCommand = sprintf('git show refs/remotes/origin/main:%s > %s', fileName, ancestor);
+    gitCommand = sprintf('git show refs/remotes/origin/main:%s > %s', fileName, ancestor)
     
-    [status, result] = system(gitCommand);
-    assert(status==0, result);
+    [status, result] = system(gitCommand)
+    assert(status==0, result)
 
 end
 
